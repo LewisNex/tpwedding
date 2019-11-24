9c9d7a52cdc1fd487ccedbd4c98aebf6f84f3fda
LewisNex <lewislightnex@gmail.com>
2019-11-24 10:23:35 +0000

    test

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
-
 # Serve static page
 @app.route('/', defaults={'path': ''}, methods=['GET'])
 @app.route('/<path:path>')
